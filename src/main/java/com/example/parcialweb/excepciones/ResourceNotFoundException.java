7e759970a5d8d000a4b0b1290ebdf519d7f1a59f
Axell5 <axelcaro034@gmail.com>
2024-05-21 17:52:22 -0500

    Initial commit

---
+++ src/main/java/com/example/parcialweb/excepciones/ResourceNotFoundException.java
@@ -0,0 +1,2 @@
+package com.example.parcialweb.excepciones;public class ResourceNotFoundException {
+}
---
+++ src/main/java/com/example/parcialweb/repositorio/CelularRepositorio.java
@@ -0,0 +1,2 @@
+package com.example.parcialweb.repositorio;public interface CelularRepositorio {
+}
